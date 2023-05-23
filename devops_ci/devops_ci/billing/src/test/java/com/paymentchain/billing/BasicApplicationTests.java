b52de71a1eabd4749e53841631183f3d3b3473d9
esotelo <ejsotelo4@gmail.com>
2023-05-23 03:53:04 +0000

    Nuevo cambio en test

--- devops_ci/devops_ci/billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ devops_ci/devops_ci/billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
  
  @Test
  public void contextLoads() {
- 	    String meessage= "default message cambio test devops_1";
+ 	    String meessage= "default message cambio test devops_2";
  	    Assert.assertNotNull(meessage);
  }
  
